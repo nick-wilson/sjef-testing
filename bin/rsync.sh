12e28f1b294d9ea2ab25dffd93e09da3ecfffa94
Nick Wilson <12475993+nick-wilson@users.noreply.github.com>
2022-11-18 16:37:32 +0000

    setup files

--- bin/rsync.sh
+++
@@ -1,4 +0,0 @@
-rsync -av --exclude=/AppData --exclude=/OneDrive* --exclude=/anaconda3 --exclude=/vcpkg --exclude=/trash /c/Users/nick/ marat:backups/windows/ --max-size=1m --delete-excluded
-rsync -av /home/nick/ marat:backups/windows.home.nick/
-rsync -av --exclude=/AppData --exclude=/OneDrive* --exclude=/anaconda3 --exclude=/vcpkg --exclude=/trash /c/Users/nick/ marat:backups/windows/ --max-size=1g --delete-excluded
-rsync -av --exclude=/AppData --exclude=/OneDrive* --exclude=/anaconda3 --exclude=/vcpkg --exclude=/trash /c/Users/nick/ marat:backups/windows/ --delete-excluded
